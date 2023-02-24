ffa4d3a69cca6681d11b87d3e9f89fec55f82569
shuhsuan <shuhsuanchang.9@gmail.com>
2023-02-24 21:39:16 +0800

    Just need to get separate displays

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 function App() {
-  const [secondsS, setSecondsS] = React.useState(5);
-  const [minutesS, setMinutesS] = React.useState(0);
+  const [secondsS, setSecondsS] = React.useState(0);
+  const [minutesS, setMinutesS] = React.useState(25);
   const [secondsB, setSecondsB] = React.useState(0);
   const [minutesB, setMinutesB] = React.useState(5);
   const [status, setStatus] = React.useState("off");
-  const [session, setSession] = React.useState("session");
+  const [session, setSession] = React.useState("Session");
+  const [displaySec, setDisplaySec] = React.useState(secondsS);
+  const [displayMin, setDisplayMin] = React.useState(minutesS);
 
-  
-    React.useEffect(() => {
-      if (session == "session") {
-      if (status == "on") {
+  React.useEffect(() => {
+    if (status == "on") {
+      if (session == "Session") {
         const count = setInterval(() => {
           setSecondsS((prevSecondsS) => prevSecondsS - 1);
         }, 1000);
-  
+
         return () => clearInterval(count);
-      }
-    }
-    else{
-      if (status == "on") {
+      } else {
         const count = setInterval(() => {
           setSecondsB((prevSecondsB) => prevSecondsB - 1);
         }, 1000);
@@ -26,10 +24,19 @@ function App() {
         return () => clearInterval(count);
       }
     }
-    }, [status]);
-  
-    
-  
+  }, [status, session]);
+
+  React.useEffect(() => {
+    if (session == "Session") {
+      setDisplaySec(secondsS);
+
+      setDisplayMin(minutesS);
+    } else {
+      setDisplaySec(secondsB);
+
+      setDisplayMin(minutesB);
+    }
+  });
 
   const handleClick = () => {
     if (status == "off") {
@@ -61,34 +68,63 @@ function App() {
 
   function checkZ() {
     if (secondsS == -1) {
-      if (session == "session") {
-        setMinutesS((prevMinutesS) => prevMinutesS - 1);
-        setSecondsS(59);
-      } else {
-        setMinutesB((prevMinutesB) => prevMinutesB - 1);
-        setSecondsB(59);
-      }
-    } else if (secondsS == 0 && minutesS == 0) {
-      setSession("break");
-    } else if (secondsB == 0 && minutesB == 0 && session == "break") {
+      setMinutesS((prevMinutesS) => prevMinutesS - 1);
+      setSecondsS(59);
+    } else if (secondsB == -1) {
+      setMinutesB((prevMinutesB) => prevMinutesB - 1);
+      setSecondsB(59);
+    } else if (secondsS == 0 && minutesS == 0 && session == "Session") {
+      setSession("Break");
+    } else if (secondsB == 0 && minutesB == 0 && session == "Break") {
       clearInterval(count);
     }
   }
 
+  const reset = () => {
+    setMinutesB(5);
+    setMinutesS(25);
+    setSecondsS(0);
+    setSecondsB(0);
+    setStatus("off");
+  }
+
   return (
     <div id="container" className="text-center">
+      {checkZ()}
       <h1>{minutesS + ":" + secondsS}</h1>
       <h1>{minutesB + ":" + secondsB}</h1>
-      {checkZ()}
-      <button onClick={handleClick}>Start/Stop</button>
+      <div id="time-left">{session}</div>
+      <h1>{displayMin + ":" + displaySec}</h1>
+
+      <button id="start_stop" onClick={handleClick}>
+        Start/Stop
+      </button>
+
       <div id="session">
-        <button onClick={addMinuteS}>Session minute +</button>
-        <button onClick={subtractMinuteS}>Session minute -</button>
+        <div id="session-label">Session Length</div>
+        <div id="session-length">25</div>
+        <button id="session-increment" onClick={addMinuteS}>
+          Session minute +
+        </button>
+        <button id="session-decrement" onClick={subtractMinuteS}>
+          Session minute -
+        </button>
       </div>
+
       <div id="break">
-        <button onClick={addMinuteB}>Break minute +</button>
-        <button onClick={subtractMinuteB}>Break minute -</button>
+        <div is="break-label">Break Length</div>
+        <div id="break-length">5</div>
+        <button id="break-increment" onClick={addMinuteB}>
+          Break minute +
+        </button>
+        <button id="break-decrement" onClick={subtractMinuteB}>
+          Break minute -
+        </button>
       </div>
+      <br />
+      <button id="reset" onClick={reset}>
+        Reset
+      </button>
     </div>
   );
 }
